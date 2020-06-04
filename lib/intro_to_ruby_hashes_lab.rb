c3a3db3a71154d66eedeca0e2456353127fc9cbb
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-04 20:42:51 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,26 @@
 def new_hash
   # return an empty hash
+  grades = Hash.new
 end
 
 def my_hash
+  name = {
+    name: 'Kelsey'
+  }
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  name = {
+    name: "Grace Hopper"
+  }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  id = {
+  id: number
+  }
   # return a hash with a key :id assigned to the provided number
 end
